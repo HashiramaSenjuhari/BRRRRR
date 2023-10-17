095216f3979ee808618f13c8261546715d7323a3
Hariprasath <truebillionhari@gmail.com>
2023-10-17 11:40:20 +0530

    changes done

---
+++ src/app.js
@@ -0,0 +1,167 @@
+// Write your code on this file. Please don't change the existing code
+// unless absolutely needed.
+
+//Initial price of the burger
+var wholeWheatBun = 10;
+
+//Ingredients of the burger along with the price
+// Clue: the name is same as the textcontent of the button. Will be useful later on :)
+var ingredients = {
+  Patty: 80,
+  Cheese: 10,
+  Tomatoes: 20,
+  Onions: 20,
+  Lettuce: 20
+};
+
+//Current state of the ingredients in the burger
+var state = {
+  Patty: true,
+  Cheese: true,
+  Tomatoes: true,
+  Onions: true,
+  Lettuce: true
+};
+
+// This function renders the entire screen everytime the state changes accordingly
+function renderAll() {
+  renderPatty();
+  renderCheese();
+  renderTomatoes();
+  renderOnions();
+  renderLettuce();
+  renderButtons();
+  renderIngredientsBoard();
+  renderPrice();
+}
+
+function renderPatty() {
+  let patty = document.querySelector("#patty");
+  //you can also use getElementById
+  if (state.Patty) {
+    patty.style.display = "inherit";
+  } else {
+    patty.style.display = "none";
+  }
+}
+
+function renderCheese() {
+ let cheese = document.querySelector('#cheese');
+ if (state.Cheese){
+  cheese.style.display = 'inherit';
+ }else{
+  cheese.style.display = 'none';
+ }
+}
+
+ function renderTomatoes() {
+   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+   let tomatoes = document.querySelector('#tomato');
+  if (state.Tomatoes){
+   tomatoes.style.display = 'inherit';
+  }else{
+   tomatoes.style.display = 'none';
+  }
+}
+
+ function renderOnions() {
+ //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onion = document.querySelector('#onion');
+ if (state.Onions){
+  onion.style.display = 'inherit';
+ }else{
+  onion.style.display = 'none';
+  }
+}
+
+ function renderLettuce() {
+   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector('#lettuce');
+  if (state.Lettuce){
+   lettuce.style.display = 'inherit';
+  }else{
+   lettuce.style.display = 'none';
+  }
+}
+
+document.querySelector(".btn-patty").onclick = function () {
+  state.Patty = !state.Patty;
+  renderAll();
+};
+
+// Trial 2 - Setup event listener for the cheese button
+document.querySelector('.btn-cheese').onclick = function(){
+  state.Cheese = !state.Cheese;
+  renderAll();
+;}
+
+// // Trial 2 - Setup event listener for the tomatoes button
+document.querySelector('.btn-tomatoes').onclick = function(){
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+;}
+
+// // Trial 2 - Setup event listener for the onion button
+document.querySelector('.btn-onions').onclick = function(){
+  state.Onions = !state.Onions;
+  renderAll();
+;}
+
+// // Trial 2 - Setup event listener for the lettuce button
+
+document.querySelector('.btn-lettuce').onclick = function(){
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+;}
+//Challenge 1 - Add/Remove the class active to the buttons based on state
+function renderButtons() {
+  // Iterate over the ingredient buttons
+  for (const ingredient in state) {
+    const button = document.querySelector(`.btn-${ingredient}`);
+    if (state[ingredient]) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  }
+}
+
+
+//Challenge 2 - Render only the items selected in the ingredients board based on the state
+
+function renderIngredientsBoard() {
+  const ingredientsBoard = document.querySelector(".menu-container");
+  ingredientsBoard.innerHTML = "<h3>Ingredients</h3>";
+
+  for (const ingredient in state) {
+    if (state[ingredient]) {
+      const pElement = document.createElement("p");
+      pElement.classList.add("items");
+      pElement.textContent = ingredient;
+      ingredientsBoard.appendChild(pElement);
+    }
+  }
+}
+
+//Judgement 1
+//In the p element having price-details as the class, display the calculated
+//price based on ingredients
+// Calculate the burger price based on the selected ingredients
+function calculatePrice() {
+  let price = wholeWheatBun; 
+
+  for (const ingredient in state) {
+    if (state[ingredient]) {
+      price += ingredients[ingredient];
+    }
+  }
+
+  return price;
+}
+
+function renderPrice() {
+  const priceElement = document.querySelector(".price-details");
+  priceElement.textContent = `Price: $${calculatePrice()}`;
+}
+
+
